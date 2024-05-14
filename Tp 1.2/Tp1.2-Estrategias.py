2b5261c4bf57be7e141a8cf3c0b58866d72d0a03
Tiago Pieroni <tepieroni@gmail.com>
2024-05-14 19:07:54 -0300

    correcciones

--- Tp 1.2/Tp1.2-Estrategias.py
+++ Tp 1.2/Tp1.2-Estrategias.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 
 # Ruleta
 
-
 def ruleta():
     valor = random.randint(0, 36)
     return valor
@@ -15,18 +14,26 @@ def ruleta():
 def grafica_balance(balanceArray, i, strategy_name):
 
     plt.figure(figsize=(10, 6))
-    plt.plot(range(1, len(balanceArray) + 1),
+    plt.plot(range(0, len(balanceArray)),
              balanceArray, label='Flujo de ganancia', color='blue')
-    plt.axhline(0, color='red', linestyle='--',
-                linewidth=2, label='Balance cero')
+    if balanceArray[0] == 0:
+        plt.title('Gráfico de ganancia neta - Corrida ' + str(i+1) + ' - Tiradas: ' +
+              str(len(balanceArray)-1)+' - Estrategia ' + strategy_name)
+        plt.axhline(balanceArray[0], color='red', linestyle='--',linewidth=2, label='Balance cero')
+        plt.axhline(balanceArray[-1], color='green', linestyle=':',linewidth=2, label='Balance final')
+    else:
+        plt.title('Gráfico de flujo de capital - Corrida ' + str(i+1) + ' - Tiradas: ' +
+              str(len(balanceArray)-1)+' - Estrategia ' + strategy_name)
+        plt.axhline(balanceArray[0], color='red', linestyle='--',linewidth=2, label='Capital inicial')
+        plt.axhline(balanceArray[-1], color='green', linestyle=':',linewidth=2, label='Capital final')
+    
     plt.xlabel('Número de tirada')
     plt.ylabel('Cantidad de ganancia')
-    plt.title('Gráfico de ganancia neta - Corrida ' + str(i+1) + ' - Tiradas: ' +
-              str(len(balanceArray))+' - Estrategia ' + strategy_name)
+
     plt.legend()
     plt.grid(True)
     # Límites del eje x de tirada 1 a la 10
-    plt.xlim(1, len(balanceArray))
+    plt.xlim(0, len(balanceArray))
     # Guardar la figura en disco
     plt.savefig('balance_corrida_'+str(i+1)+'.png')
     plt.show()  # Mostrar la figura
@@ -44,21 +51,23 @@ def martingala_strategy(initial_bet, cant_tiradas, initial_capital, capital):
     betArray.append(initial_bet)
     for i in range(cant_tiradas):
         if betArray[i] <= balanceArray[i] or balanceArray[0] == 0:
-            print("tirada: ", i)
+            print("tirada: ", (i+1))
             print("bet: ", betArray[i])
             print("capital: ", balanceArray[i])
             valor = ruleta()
             if valor in negro:
                 print("Gano")
+                print()
                 balanceArray.append(balanceArray[i] + betArray[i])
                 betArray.append(betArray[0])
             else:
                 print("Perdio")
+                print()
                 balanceArray.append(balanceArray[i] - betArray[i])
                 betArray.append(betArray[i] * 2)
+            print(balanceArray)
         else:
             print("Te quedaste seco")
-            balanceArray.append(0)
             break
 
     return balanceArray, betArray
@@ -200,26 +209,22 @@ capital = (sys.argv[10])
 negro = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 
 # Parametros ingresados por la consola
-initial_bet = int(input("Ingrese la apuesta inicial: "))
+
 if capital == "a":
-    initial_capital = int(input("Ingrese la capital inicial: "))
+    initial_capital = int(input("Ingrese el capital inicial: "))
 else:
     initial_capital = math.inf
     print(initial_capital)
+initial_bet = int(input("Ingrese la apuesta inicial: "))
 
 if (estrategia) == "m":
     # Simulación de la estrategia de Martingala
-    simulate_game(martingala_strategy, initial_bet,
-                  cant_tiradas, cant_corridas, initial_capital, capital, "Martin Gala")
+    simulate_game(martingala_strategy, initial_bet, cant_tiradas, cant_corridas, initial_capital, capital, "Martin Gala")
 elif (estrategia) == "d":
     # Simulación de la estrategia de D'Alembert
-    simulate_game(dalembert_strategy, initial_bet,
-                  cant_tiradas, cant_corridas, initial_capital, capital, "D'Alembert")
+    simulate_game(dalembert_strategy, initial_bet, cant_tiradas, cant_corridas, initial_capital, capital, "D'Alembert")
 elif (estrategia) == "f":
     # Simulación de la estrategia de Fibonacci
-    simulate_game(fibonacci_strategy, initial_bet, cant_tiradas,
-                  cant_corridas, initial_capital, capital, "Fibonacci")
-else:
-    print("else")
-    simulate_game(propia_strategy, initial_bet,
-                  cant_tiradas, cant_corridas, initial_capital, capital, "Nosotros")
+    simulate_game(fibonacci_strategy, initial_bet, cant_tiradas, cant_corridas, initial_capital, capital, "Fibonacci")
+elif (estrategia) == "o":
+    simulate_game(paroli_strategy, initial_bet, cant_tiradas, cant_corridas, initial_capital, capital, "Nosotros")
