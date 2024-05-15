25d7769d70517278bada3e7d6454edda06712c4f
Mateo Tosello <tosellomateo0@gmail.com>
2024-05-15 09:33:39 -0300

    Corregimos apuestas por color

--- Tp 1.2/Tp1.2-Estrategias.py
+++ Tp 1.2/Tp1.2-Estrategias.py
@@ -21,7 +21,7 @@ def grafica_balance(balanceArray, i, strategy_name):
 
     if balanceArray[0] == 0:
         plt.plot(range(0, len(balanceArray)),
-            balanceArray, label='Flujo de ganancia', color='blue')
+                 balanceArray, label='Flujo de ganancia', color='blue')
         plt.title('Gráfico de flujo de ganancia - Corrida ' + str(i+1) + ' - Tiradas: ' +
                   str(len(balanceArray)-1)+' - Estrategia ' + strategy_name)
         plt.axhline(balanceArray[0], color='red',
@@ -31,7 +31,7 @@ def grafica_balance(balanceArray, i, strategy_name):
         plt.ylabel('Ganancia')
     else:
         plt.plot(range(0, len(balanceArray)),
-            balanceArray, label='Flujo de capital', color='blue')
+                 balanceArray, label='Flujo de capital', color='blue')
         plt.title('Gráfico de flujo de capital - Corrida ' + str(i+1) + ' - Tiradas: ' +
                   str(len(balanceArray)-1)+' - Estrategia ' + strategy_name)
         plt.axhline(balanceArray[0], color='red',
@@ -45,7 +45,7 @@ def grafica_balance(balanceArray, i, strategy_name):
     plt.grid(True)
     plt.xlim(0, len(balanceArray))
     plt.savefig('balance_corrida_'+str(i+1)+'.png')
-    plt.show() 
+    plt.show()
 
 
 def grafica_resumen_balances(balancesArray, i, strategy_name):
@@ -56,37 +56,46 @@ def grafica_resumen_balances(balancesArray, i, strategy_name):
     for i in range(len(balancesArray)):
         if cant_max_tiradas < len(balancesArray[i]):
             cant_max_tiradas = len(balancesArray[i])
-        if balancesArray[0][0]==0:
+        if balancesArray[0][0] == 0:
             plt.plot(nro_tiradas_eje_x[:len(balancesArray[i])],
-                 balancesArray[i], label='Flujo de ganancia corrida '+str(i+1), color='C'+str(i % 10+1))
+                     balancesArray[i], label='Flujo de ganancia corrida '+str(i+1), color='C'+str(i % 10+1))
             plt.axhline(balancesArray[i][-1], color='C'+str(i % 10+1),
-                linestyle=':', linewidth=2) #, label='Balance final corrida '+str(i+1))
-        else:  
+                        # , label='Balance final corrida '+str(i+1))
+                        linestyle=':', linewidth=2)
+        else:
             plt.plot(nro_tiradas_eje_x[:len(balancesArray[i])],
-                 balancesArray[i], label='Flujo de capital corrida '+str(i+1), color='C'+str(i % 10+1))
+                     balancesArray[i], label='Flujo de capital corrida '+str(i+1), color='C'+str(i % 10+1))
             plt.axhline(balancesArray[i][-1], color='C'+str(i % 10+1),
-                linestyle=':', linewidth=2) #, label='Capital final corrida '+str(i+1))
-    
-    if balancesArray[0][0]==0:
-        plt.axhline(0, color='red', linestyle='--', linewidth=2, label='Balance cero')
+                        # , label='Capital final corrida '+str(i+1))
+                        linestyle=':', linewidth=2)
+
+    if balancesArray[0][0] == 0:
+        plt.axhline(0, color='red', linestyle='--',
+                    linewidth=2, label='Balance cero')
         plt.ylabel('Ganancia')
         plt.title('Gráfico de flujo de ganancia - Cantidad de Corridas ' + str(i+1) + ' - Tiradas máximas: ' +
-              str(cant_max_tiradas-1)+' - Estrategia ' + str(strategy_name))
+                  str(cant_max_tiradas-1)+' - Estrategia ' + str(strategy_name))
     else:
-        plt.axhline(balancesArray[0][0], color='red', linestyle='--', linewidth=2, label='Capital inicial')
+        plt.axhline(balancesArray[0][0], color='red',
+                    linestyle='--', linewidth=2, label='Capital inicial')
         plt.ylabel('Capital')
         plt.title('Gráfico de flujo de capital - Cantidad de Corridas ' + str(i+1) + ' - Tiradas máximas: ' +
-              str(cant_max_tiradas-1)+' - Estrategia ' + str(strategy_name))
-        
-   
+                  str(cant_max_tiradas-1)+' - Estrategia ' + str(strategy_name))
+
     plt.xlabel('Número de tirada')
     plt.legend()
     plt.grid(True)
     plt.xlim(0, cant_max_tiradas-1)
     plt.savefig('balance_resumen.png')
-    plt.show() 
+    plt.show()
+
+
+def grafica_frec_relativa(betArray):
+    plt.hist(betArray, edgecolor='black')
+    print("Aca va la grafica")
 
 # Estrategias
+
 
 def martingala_strategy(initial_bet, cant_tiradas, initial_capital, capital):
     balanceArray = []
@@ -102,7 +111,7 @@ def martingala_strategy(initial_bet, cant_tiradas, initial_capital, capital):
             print("bet: ", betArray[i])
             print("capital: ", balanceArray[i])
             valor = ruleta()
-            if valor in negro:
+            if valor in color:
                 print("Gano")
                 print()
                 balanceArray.append(balanceArray[i] + betArray[i])
@@ -134,7 +143,7 @@ def dalembert_strategy(initial_bet, cant_tiradas, initial_capital, capital):
         print("Bet: ", betArray[i])
         if betArray[i] <= balanceArray[i]:
             valor = ruleta()
-            if valor in negro:
+            if valor in color:
                 print("Gano")
                 balanceArray.append(balanceArray[i] + betArray[i])
                 # para que no llegue a apuesta 0 en caso de ganar
@@ -170,7 +179,7 @@ def fibonacci_strategy(initial_bet, cant_tiradas, initial_capital, capital):
         print("Array: ", valores)
         if valores[1] <= balanceArray[i]:
             valor = ruleta()
-            if valor in negro:
+            if valor in color:
                 print("Gano")
                 balanceArray.append(balanceArray[i] + valores[1])
                 if valores[0] == 0 or valores[0] == initial_bet:
@@ -210,7 +219,7 @@ def paroli_strategy(initial_bet, cant_tiradas, initial_capital, capital):
     for i in range(cant_tiradas):
         if betArray[i] <= balanceArray[i]:
             valor = ruleta()
-            if valor in negro:
+            if valor in color:
                 print("Gano")
                 balanceArray.append(balanceArray[i] + betArray[i])
 
@@ -257,7 +266,11 @@ num_elegido = int(sys.argv[6])
 estrategia = (sys.argv[8])
 capital = (sys.argv[10])
 
-negro = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+if num_elegido == "n":
+    color = [2, 4, 6, 8, 10, 11, 13, 15, 17,
+             20, 22, 24, 26, 28, 29, 31, 33, 35]
+else:
+    color = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
 
 # Parametros ingresados por la consola
 
